025bf294a819a13957f5954d4edbc569c56c0bc0
daniil934 <nysch.dan@gmail.com>
2023-02-17 02:28:34 +0200

    added

--- url_shortener_app/app/controllers/urls_controller.rb
+++ url_shortener_app/app/controllers/urls_controller.rb
@@ -11,7 +11,7 @@ class UrlsController < ApplicationController
   def create
     @url = Url.new(url_params)
     if @url.save
-      #
+      @url.update_attribute(:short_url, generate_short_url)
     else
       render 'new'
     end
@@ -27,6 +27,10 @@ class UrlsController < ApplicationController
     private
 
     def url_params
-      params.require(:url).permit(:full_url)
+      params.require(:url).permit(:full_url, :short_url)
+    end
+
+    def generate_short_url
+      SecureRandom.uuid[0..5] 
     end
 end
